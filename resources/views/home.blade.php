3d1dcaa3bc97a51baa5a6a62f52e8e6c85fb5d16
Naddy-Hazel <nadia.kasri@icu.gov.my>
2024-10-15 10:42:38 +0800

    Day 2 Learning

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>{{ config('app.name') }}</title>
 </head>
 <body>
-    <h1>Hello youu!</h1>
+    <h1>Hello youu! Home, {{ $name ?? 'Alia' }} </h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,54 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home{name}', function ($name) {
+    return view('home', ['name'=> $name]);
+});
+
 Route::get('/home', function () {
     return view('home');
 });
 
 Route::get('/auth/signin', function () {
     return view('auth.signin');
+});
+
+
+
+//Name Route
+Route::get('/user/profile', function () {
+    return 'Pengguna Profile';
+} ) ->name('user.profile');
+//Route Param
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+} );
+
+
+//alias of a route user.profile = /pengguna/profile
+
+//redirect route to name route
+Route::get('/redirect-to-profile', function () {
+    return 'Pengguna Profiles';
+} ) ->name('user.profile');
+
+//route group - without repeat the type of food
+Route::prefix('food')->group(function () {
+    Route::get('/details',function () {
+        return 'Food details are following';
+    });
+    Route::get('/home',function () {
+        return 'Food home page';
+    });
+} );
+
+//Combination of all
+Route::name('job')->prefix('job')->group (function(){
+    Route::get('home',function () {
+        return 'Job Home Page';
+    })->name('.home');
+
+    Route::get('/details',function () {
+        return 'Job details are following';
+    })->name('.description');
 });
